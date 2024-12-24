40da7d1ea387f57ae6832d1145d687933e3784be
Muhammad Tarek <mohammadtarek000@gmail.com>
2024-12-24 05:00:14 +0200

    make tab accepts suggestions

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -23,34 +23,9 @@ return {
         ["<C-k>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
         ["<C-j>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
 
-        -- Handle Tab for LSP and Snippets
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          -- If completion menu is visible, select and confirm the current item
-          if cmp.visible() then
-            cmp.confirm { select = true } -- Automatically confirm the current selected item
-          -- If a snippet can be jumped, use it
-          elseif luasnip.jumpable(1) then
-            luasnip.jump(1)
-          else
-            fallback() -- Default Tab behavior (e.g., insert space)
-          end
-        end, { "i", "s" }),
-
-        -- Handle Shift+Tab for LSP and Snippets
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          -- If completion menu is visible, navigate to the previous suggestion
-          if cmp.visible() then
-            cmp.select_prev_item { behavior = cmp.SelectBehavior.Select }
-          -- If a snippet can be jumped backwards, use it
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback() -- Default Shift-Tab behavior
-          end
-        end, { "i", "s" }),
-
         -- Optionally, you can keep Enter to confirm the current item explicitly
         ["<CR>"] = cmp.mapping.confirm { select = true },
+        ["<Tab>"] = cmp.mapping.confirm { select = true },
       },
     })
   end,
